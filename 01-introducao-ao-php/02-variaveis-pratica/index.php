fd8c0f142357ac668497f25bb46d0ebe4b133ff2
Icaro Miguel da Silva Costa <icaromigueldasilvacosta@gmail.com>
2026-08-13 21:41:54 -0300

    adicionar pratica com variaveis

---
+++ 01-introducao-ao-php/02-variaveis-pratica/index.php
@@ -0,0 +1,28 @@
+<?php
+
+$nome = "Ícaro Miguel";
+$anoDeNascimento = 2008;
+$curso = "Programador Web";
+
+$temTatuagem = false;
+
+
+
+
+echo "Meu nome é {$nome} e eu faço o curso de {$curso}";
+
+var_dump($temTatuagem);
+
+
+
+// Exercicío Prático
+
+$nomeDoFilme = "Vingadores - Guerra Infinita";
+$genero = "Ficção Científica";
+$anoDeLancamento = 2018;
+$sinopse = "É um filme de super herois da marvel";
+
+echo "<h2>{$nomeDoFilme}</h2>";
+echo "<h3>{$genero}</h3>";
+echo "<p>{$anoDeLancamento}</p>";
+echo "<p>{$sinopse}</p>";
