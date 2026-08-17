64d93be8581fc10fab31ff49911265dd82d39029
Icaro Miguel da Silva Costa <icaromigueldasilvacosta@gmail.com>
2026-08-17 19:42:22 -0300

    adiconar aulas-04-operadores-relacionaveis

--- 01-introducao-ao-php/02-variaveis-pratica/index.php
+++ 01-introducao-ao-php/02-variaveis-pratica/index.php
@@ -20,7 +20,7 @@ var_dump($temTatuagem);
 $nomeDoFilme = "Vingadores - Guerra Infinita";
 $genero = "Ficção Científica";
 $anoDeLancamento = 2018;
-$sinopse = "É um filme de super herois da marvel";
+$sinopse = "É um filme de super heróis da Marvel";
 
 echo "<h2>{$nomeDoFilme}</h2>";
 echo "<h3>{$genero}</h3>";
